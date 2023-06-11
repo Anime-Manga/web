d8c4016734ba818fac190d0b885d04e924f3e650
Cesxhin <mattia.ceschin@gmail.com>
2023-06-11 16:20:44 +0200

    Now is protected the cookie

--- src/composables/useAuth.js
+++ src/composables/useAuth.js
@@ -1,8 +1,24 @@
+import cry from 'crypto-js';
+
 export default function useAuth(){
 
-    function createCookie(data){        
-        const auth = useCookie('auth');
-        auth.value = data;
+    function createCookie(data){
+
+        const date = new Date();
+        date.setFullYear(date.getFullYear() + 1);
+        
+        console.log(date);
+        const auth = useCookie('auth', {
+            expires: date,
+            priority: 'high'
+        });
+
+        try{
+            const token = cry.AES.encrypt(JSON.stringify(data), 'test').toString();
+            auth.value = token;
+        }catch(err){
+            console.log(err);
+        }
     }
 
     function deleteCookie(){
@@ -11,10 +27,19 @@ export default function useAuth(){
     }
 
     function getCookie(){
-        return useCookie('auth', {
-            maxAge: 86400 * 10,
+        
+        const date = new Date();
+        date.setFullYear(date.getFullYear() + 1);
+
+        const token =  useCookie('auth', {
+            expires: date,
             priority: 'high'
         });
+
+        if(!isNil(token.value))
+            return  JSON.parse(cry.AES.decrypt(token.value, 'test').toString(cry.enc.Utf8));
+        else
+            return null;
     }
     
     return{
